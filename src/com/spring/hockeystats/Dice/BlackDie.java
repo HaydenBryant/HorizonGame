c611e9349d76a29c6d7a5f171d6f3b61fa7080d6
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-11-25 22:55:55 -0500

    changing dice handling. cant find good way to set this up compared to other packages

--- src/com/spring/hockeystats/Dice/BlackDie.java
+++ src/com/spring/hockeystats/Dice/BlackDie.java
@@ -1,13 +1,32 @@
 package com.spring.hockeystats.Dice;
 
-public class BlackDie extends Die{
-    public BlackDie(int sides) {
-        super(sides, "Black");
+import java.util.Random;
+
+public class BlackDie implements Dice{
+    private int sides;
+    private String type;
+    private int value;
+
+    Random rand = new Random();
+
+    public BlackDie(int sides, String type) {
+        this.sides = 6;
+        this.type = "Black";
+    }
+
+    @Override
+    public int roll() {
+        return 0;
     }
 
     @Override
     public int getValue() {
-        return super.getValue();
+        return 0;
+    }
+
+    @Override
+    public String getType() {
+        return null;
     }
 
 }
--- src/com/spring/hockeystats/Dice/OrangeDie.java
+++ src/com/spring/hockeystats/Dice/OrangeDie.java
@@ -1,13 +1,32 @@
 package com.spring.hockeystats.Dice;
 
-public class OrangeDie extends Die{
-    public OrangeDie(int sides) {
-        super(sides, "Orange");
+import java.util.Random;
+
+public class OrangeDie implements Dice{
+    private int sides;
+    private String type;
+    private int value;
+
+    Random rand = new Random();
+
+    public OrangeDie(int sides, String type) {
+        this.sides = 6;
+        this.type = "Orange";
+    }
+
+    @Override
+    public int roll() {
+        return 0;
     }
 
     @Override
     public int getValue() {
-        return super.getValue();
-
+        return 0;
     }
+
+    @Override
+    public String getType() {
+        return null;
+    }
+
 }
